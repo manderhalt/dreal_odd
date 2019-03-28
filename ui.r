116de8f4db016e2feff8db7cbc3368a78243c0d1
Axel MARCHAND <amarchand@Axels-MacBook-Pro.local>
2019-03-29 00:09:06 +0100

    AXEL refactor of UI

--- ui.r
+++ ui.r
@@ -11,93 +11,103 @@ source("data.R")
 source("helper.R")
 
 navbarPage("DREAL Quizz",
-           tabPanel("Home",
-                    # Sidebar layout with input and output definitions ----
-                    sidebarLayout(
-                      
-                      # Sidebar panel for inputs ----
-                      sidebarPanel(
-                        
-                        # Input: Slider for the number of bins ----
-                        imageOutput("image_dreal")
-                      )
-                      ,
-                      
-                      # Main panel for displaying outputs ----
-                      mainPanel(
-                        h3(textOutput("caption")),
-                        # Output: Histogram ----
-                        textOutput("intro_text"),
-                        
-                        h4(textOutput("catchphrase")),
-                        selectInput("department", "Quel est votre département ?",DF_DEP$Zone),
-                        uiOutput("commune"),
-                        h4(textOutput("epci_text")),
-                        tags$form(
-                        lapply(1:length(QUESTION$Libel), function(question_number){
-                          current_question <- QUESTION[question_number,]
-                          logos <- logos_from_code_indic(current_question$Code_indicateur)
-                          if (length(logos)>1){
-                            img_file_1 = paste(logos[[1]],".jpg", sep="")
-                            img_file_2 = paste(logos[[2]],".jpg", sep="")
-                            div(
-                              checkboxGroupInput(inputId=paste("question_",current_question$Num_question,sep=''), 
-                                                 label=current_question$Libel, choices=CHOICES, inline=TRUE),
-                              img(src=img_file_1, width = 50), img(src=img_file_2, width = 50))
-                          }
-                          else {
-                            img_file = paste(logos[[1]],".jpg", sep="")
-                          div(
-                          checkboxGroupInput(inputId=paste("question_",current_question$Num_question,sep=''), 
-                                             label=current_question$Libel, choices=CHOICES, inline=TRUE),
-                          img(src=img_file, width = 50))
-                          }
-                        }), 
-                        br(),
-                        actionButton("submitBtn", "Submit")
-                        )
-                        ,
-                        divwheelnavOutput("nav_output")
-                        
-                        
-                        
-                      )
-                    )
-                    
-                    ),
-           tabPanel("Les ODD, qu'est ce que c'est",
-                    navlistPanel(
-                      "Les ODD en bref",
-                      tabPanel("Définition",
-                               h4(ODD_HEADER),
-                              "",
-                              ODD_BASIS),
-                      tabPanel("Les 17 ODD",
-                               lapply(1:17, function(i) {
-                                 odd <- paste("ODD ", i, sep="")
-                                 id_button <- paste("ODD_button_", i, sep="")
-                                 odd_image <- paste("ODD", i,".jpg", sep="")
-                                 tags$button(
-                                         id = id_button,
-                                         class = "btn action-button",
-                                         img(src = odd_image,
-                                             height = "75px")
-                                       )
-                                       
-                                     
-                               }
-                               ),
-                               column(width = 9, align = "center", h4(textOutput("text_odd")))
-                      )
-                    )),
            
+           # PREMIERE PAGE
+           tabPanel(
+             "Home",
+             sidebarLayout(
+               sidebarPanel(
+                 imageOutput("image_dreal")
+               )
+               ,
+               mainPanel(
+                 
+                 # TITRES ET PARAGRAPHE D'INTRO
+                 h3(textOutput("caption")),
+                 textOutput("intro_text"),
+                 h4(textOutput("catchphrase")),
+                 
+                 # SELECTION DEPARTEMENT COMMUNE
+                 selectInput("department", "Quel est votre département ?",DF_DEP$Zone),
+                 uiOutput("commune"),
+                 h4(textOutput("epci_text")),
+                 
+                 # FORM 
+                 tags$form(
+                   lapply(1:length(QUESTION$Libel), function(question_number){
+                     current_question <- QUESTION[question_number,]
+                     logos <- logos_from_code_indic(current_question$Code_indicateur)
+                     if (length(logos)>1){
+                       img_file_1 = paste(logos[[1]],".jpg", sep="")
+                       img_file_2 = paste(logos[[2]],".jpg", sep="")
+                       div(
+                         checkboxGroupInput(inputId=paste("question_",current_question$Num_question,sep=''), 
+                                            label=current_question$Libel, choices=CHOICES, inline=TRUE),
+                         img(src=img_file_1, width = 50), img(src=img_file_2, width = 50))
+                     }
+                     else {
+                       img_file = paste(logos[[1]],".jpg", sep="")
+                       div(
+                         checkboxGroupInput(inputId=paste("question_",current_question$Num_question,sep=''), 
+                                            label=current_question$Libel, choices=CHOICES, inline=TRUE),
+                         img(src=img_file, width = 50))
+                     }
+                   }), 
+                   br(),
+                   actionButton("submitBtn", "Submit")
+                 )
+                 ,
+                 
+                 # WHEEL
+                 divwheelnavOutput("nav_output") 
+               )
+             )
+             
+           ),
            
+           # DEBUT DEUXIEME PAGE
+           tabPanel(
+             "Les ODD, qu'est ce que c'est",
+             navlistPanel(
+               "Les ODD en bref",
+               
+               # PREMIERE CASE
+               tabPanel(
+                 "Définition",
+                 # TEXTE
+                 h4(ODD_HEADER),
+                 "",
+                 ODD_BASIS),
+               
+               # DEUXIEME CASE
+               tabPanel(
+                 "Les 17 ODD",
+                 # LES 17 LOGOS
+                 lapply(1:17, function(i) {
+                   odd <- paste("ODD ", i, sep="")
+                   id_button <- paste("ODD_button_", i, sep="")
+                   odd_image <- paste("ODD", i,".jpg", sep="")
+                   tags$button(
+                     id = id_button,
+                     class = "btn action-button",
+                     img(src = odd_image,
+                         height = "75px")
+                   )
+                 }
+                 ),
+                 column(width = 9, align = "center", h4(textOutput("text_odd")))
+               )
+             )
+             ),
            
+           # TROISIEME PAGE
            tabPanel("Voir les ODD de mon territoire",
+                    sidebarPanel(
                     selectInput("department_2", "Quel est votre département ?",DF_DEP$Zone),
-                    uiOutput("commune_2"),
-                    h4(textOutput("epci_text_2")),
+                    uiOutput("commune_2")),
+                   
                     mainPanel(
+                      h4(textOutput("epci_text_2")),
                       tags$style(type="text/css",
                                  ".shiny-output-error { visibility: hidden; }",
                                  ".shiny-output-error:before { visibility: hidden; }"),
@@ -119,8 +129,8 @@ navbarPage("DREAL Quizz",
                       plotOutput(outputId ="plot_graph")
                       
                     )
-                    )
-           
-           
            )
+           
+           
+)
 
